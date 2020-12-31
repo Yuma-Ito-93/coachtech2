55eb7bd65359d182c7bd513009822780a1fa5501
Yuma-Ito-93 <menpin8000@outlook.jp>
2020-12-31 09:45:25 +0900

    commit3

--- src/components/HeaderAuth.vue
+++ src/components/HeaderAuth.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="header flex">
-    <div class="left">
-      <img class="logo" src="../assets/logo.png" alt="">
-    </div>
-    <div class="right flex">
-      <p @click="$router.push('/signup')">新規登録</p>
-      <p @click="$router.push('/')">ログイン</p>
-    </div>
-  </div>
+ <div class="header flex">
+   <div class="left">
+     <img class="logo" src="../assets/logo.png"/>
+   </div>
+   <div class="right flex">
+     <p @click="$router.push('/signup')">新規登録</p>
+     <p @click="$router.push('/')">ログイン</p>
+   </div>
+ </div>
 </template>
+
 <style scoped>
 .right {
  align-items: center;
